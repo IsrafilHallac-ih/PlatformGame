dd8de058ab07d5132bf2f66c6c30c2c386d0963e
İSRAFİL HALLAÇ <83336393+IsrafilHallac-ih@users.noreply.github.com>
2023-09-25 16:22:52 +0300

    Add files via upload

---
+++ RotateObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+   
+    void Update()
+    {
+        transform.Rotate(0, 0, 90f * Time.deltaTime);
+    }
+}
